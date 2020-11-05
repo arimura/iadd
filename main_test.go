bc41b6f609f677f39f0721fe1800b6a73cb47162
arimura <kotaro_arimura@voyagegroup.com>
2020-11-05 22:13:40 +0900

    Refactor

--- main_test.go
+++ main_test.go
@@ -3,21 +3,20 @@ package main
 import "testing"
 
 func TestDown(t *testing.T) {
-	sc := newScreenContent()
-	sc.worktreeGroup.statusLines = []statusLine{
+	sc := NewScreenContent()
+	sc.worktreeGroup.statusLines = []StatusLine{
 		{statusCode: 'm', file: "hoge"},
 		{statusCode: 'm', file: "fuga"},
 	}
-	sc.untrackingGroup.statusLines = []statusLine{
+	sc.untrackingGroup.statusLines = []StatusLine{
 		{statusCode: '?', file: "foo"},
 		{statusCode: '?', file: "bar"},
 	}
-	sc.decideInitialCusor()
 	if sc.currentGroup != 1 {
 		t.Errorf("currentGroup != 1: %d %d", sc.currentGroup, sc.currentIdx)
 	}
-	sc.down()
-	sc.down()
+	sc.Down()
+	sc.Down()
 
 	if sc.currentGroup != 2 {
 		t.Errorf("currentGroup != 2: %d %d", sc.currentGroup, sc.currentIdx)
